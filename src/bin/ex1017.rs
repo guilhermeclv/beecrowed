3730d15e9ba469c3b38ddf9c3c5e456aa96afb7c
guilhermeclv <guilhermeclv@hotmail.com>
2024-01-26 08:52:11 -0300

    refactor: using constant

--- src/bin/ex1017.rs
+++ src/bin/ex1017.rs
@@ -5,7 +5,7 @@ println!("Total de Litros Gastos: {:.3}", gasto_de_combustivel);
 
 fn gasto_de_combustivel(tempo:i32, velocidade:i32)-> f32
 {
-    let km_por_litro= 12.0;
-    let quantidade_de_litros=tempo as f32 * velocidade as f32 / km_por_litro;
+    const KM_POR_LITRO:f32= 12.0;
+    let quantidade_de_litros=tempo as f32 * velocidade as f32 / KM_POR_LITRO;
     quantidade_de_litros
 }
