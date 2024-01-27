e92e79a2305d91cedb3398d4c6ccecde669d65e8
guilhermeclv <guilhermeclv@hotmail.com>
2024-01-27 11:49:19 -0300

    refactor: change with the professor solution

--- src/bin/ex1019.rs
+++ src/bin/ex1019.rs
@@ -2,14 +2,10 @@ fn main() {
 let informa_tempo=informa_tempo(140153);
 println!("Horas: {:.0} Minutos: {:.0} Segundos: {}", informa_tempo.0, informa_tempo.1, informa_tempo.2);
 }
-fn informa_tempo(n:i32)-> (f32, f32, f32)
+fn informa_tempo(n:i32)-> (i32, i32, i32)
 {
-    let mut horas = n as f32 / 3600.0;
-    let mut minutos = (n as f32 / 60.0) % 60.0;
-    if minutos > 60.0 { 
-        minutos= minutos % 60.0;
-        horas = horas + (minutos /60.0);
-    }
-    let segundos = n as f32 % 60.0;
+    let horas = n / 3600;
+    let minutos = (n % 3600) / 60;
+    let segundos = (n % 3600) % 60;
     (horas, minutos, segundos)
 }
